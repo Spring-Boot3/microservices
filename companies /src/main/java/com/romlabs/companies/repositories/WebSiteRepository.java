61a0a535061f428541b5e97bdae48227b85f199b
HectorRoG <hectorr9577@gmail.com>
2025-03-19 13:52:08 -0600

    Clean

--- companies /src/main/java/com/romlabs/companies/repositories/WebSiteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.romlabs.companies.repositories;
-
-import com.romlabs.companies.entities.WebSite;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WebSiteRepository extends JpaRepository<WebSite, Long> {
-}
