146c3b8846dcaa0a75b2c60a3a5889669323605a
HectorARG <hectorr9577@gmail.com>
2025-03-30 15:56:17 -0600

    Trazabilidad distribuida (zipkin) -Habilitando observabilidad-

--- report-listener/src/main/java/com/romlab/reportListener/repositories/ReportRepository.java
+++ report-listener/src/main/java/com/romlab/reportListener/repositories/ReportRepository.java
@@ -4,6 +4,11 @@ import com.romlab.reportListener.documents.ReportDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReportRepository extends MongoRepository<ReportDocument, String> {
+
+    Optional<ReportDocument> findByContentContaining(String content);
+    
 }
